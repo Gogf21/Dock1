a18eec832e698dc7e2386be6749ba78b0c4b55db
Gogf21 <holopulas@gmail.com>
2024-12-23 21:20:50 +0000

    Create init.sql

---
+++ postgres/init.sql
@@ -0,0 +1,13 @@
+CREATE TABLE movies (
+    title varchar(100),
+    director varchar(50),
+    genre varchar(30)
+);
+
+INSERT INTO movies (title, director, genre) 
+VALUES 
+('Inception', 'Christopher Nolan', 'Science Fiction'),
+('The Shawshank Redemption', 'Frank Darabont', 'Drama'),
+('The Dark Knight', 'Christopher Nolan', 'Action'),
+('Pulp Fiction', 'Quentin Tarantino', 'Crime'),
+('Forrest Gump', 'Robert Zemeckis', 'Drama');
